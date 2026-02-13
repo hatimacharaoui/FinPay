a7d05d2671a5f9a3f156c52f0c07dd3eec545dea
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-02-13 17:49:18 +0100

    new updated changes new nn

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,7 @@ import java.time.LocalDate;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws SQLException {
-        LocalDate date = LocalDate.now();
-        Client c = new Client("zddSDFzpoiejrzd","AyoubHaziejridsdsSFDSFGedzedail.com","067489SDF73947");
-        Prestataire p = new Prestataire("Ayoub","Clinique");
-        Paiement  paiment = new Paiement(233.43,date,12);
-        p.AjouterFacture();
-        p.afficherFacture();
+
 
     }
 }
