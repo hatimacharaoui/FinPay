a2fb7901aa5e9f6eb5f309e945faf7bc24b549c8
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-02-23 11:59:32 +0000

    First commit

--- src/RapportExcel.java
+++ src/RapportExcel.java
@@ -15,8 +15,8 @@ public class RapportExcel {
                        "p.nom AS prestataire, " +
                        "COUNT(f.id_facture) AS nombre_factures, " +
                        "SUM(f.montant_total) AS total_genere, " +
-                       "SUM(pa.commission) AS total_commissions" +
-                       "FROM prestataire p" +
+                       "SUM(pa.commission) AS total_commissions " +
+                       "FROM prestataire p " +
                        "JOIN facture f ON p.id_prestataire = f.id_prestataire " +
                        "JOIN paiement pa ON f.id_facture = pa.id_facture " +
                        "GROUP BY p.id_prestataire, p.nom";
